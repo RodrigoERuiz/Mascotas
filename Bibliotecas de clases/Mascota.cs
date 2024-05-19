cb36adb1bcac05ad901553012bb59e16fcedc3d3
RodrigoERuiz <122758239+RodrigoERuiz@users.noreply.github.com>
2024-05-19 11:57:32 -0300

    primer commit

---
+++ Bibliotecas de clases/Mascota.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotecas_de_clases
+{
+    public abstract class Mascota
+    {
+        string nombre;
+        string raza;
+
+        protected Mascota(string nombre, string raza)
+        {
+            this.nombre = nombre;
+            this.raza = raza;
+        }
+
+
+        public string Nombre
+        {
+            get{return nombre;}
+        }
+
+        public string Raza
+        {
+            get { return raza;}
+        }
+        //Ultimos cambios la codigo dasdas dasdasdasd ad asasd as
+    }
+
+
+}
